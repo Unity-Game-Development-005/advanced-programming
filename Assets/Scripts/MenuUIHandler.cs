a1fd4d7d8a20f7b2f29391b98151291bb139cfb3
Andrew W. F. Hatton <7912262+Wanottah@users.noreply.github.com>
2025-06-24 11:34:00 +0100

    Lab 8.2
    Steps 1 - 8

--- Assets/Scripts/MenuUIHandler.cs
+++ Assets/Scripts/MenuUIHandler.cs
@@ -1,7 +1,22 @@
+
 using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+
+// enable scene management
+using UnityEngine.SceneManagement;
+
+
+// allows the application to quit if we are using the usnity editor
+#if UNITY_EDITOR
+
+using UnityEditor;
+
+#endif
+
+
 
 // Sets the script to be executed later than all default scripts
 // This is helpful for UI, since other things may need to be initialized before setting the UI
@@ -9,6 +24,12 @@ using UnityEngine;
 public class MenuUIHandler : MonoBehaviour
 {
     public ColorPicker ColorPicker;
+
+
+    // reference to 'main' scene
+    private int sceneToLoad = 1;
+
+
 
     public void NewColorSelected(Color color)
     {
@@ -21,4 +42,34 @@ public class MenuUIHandler : MonoBehaviour
         //this will call the NewColorSelected function when the color picker have a color button clicked.
         ColorPicker.onColorChanged += NewColorSelected;
     }
-}
+
+    
+    // loads a new scene
+    public void LoadScene()
+    {
+        SceneManager.LoadScene(sceneToLoad);
+    }
+
+
+    // quits the application
+    public void QuitApplication()
+    {
+
+// if we are using the unity editor
+#if UNITY_EDITOR
+
+        // quit the application from the editor
+        EditorApplication.ExitPlaymode();
+
+// otherwise
+#else
+
+        // quit the application as normal
+        Application.Quit();
+
+#endif
+
+    }
+
+
+} // end of class
